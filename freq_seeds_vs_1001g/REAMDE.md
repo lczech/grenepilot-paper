d1402ac67714d4e1d0dd898d4037d0d634b07f7d
Lucas Czech <lczech@memex.carnegiescience.edu>
2021-09-20 17:47:22 -0700

    Re-run freq_seeds_vs_1001g to usee mpileup frequencies instead of vcf-based frequencies

--- freq_seeds_vs_1001g/REAMDE.md
+++ freq_seeds_vs_1001g/REAMDE.md
@@ -1,18 +1,12 @@
-Seed freqs were extracted using the genesis app seed_freqs,
-with the command ./seed_freqs /lustre/scratch/lczech/grenepipe-runs/ath-evo-seeds-rerun/annotated/all.vcf.gz
-The resulting file is called seed_freqs.csv
+Seed freqs were extracted using grenedalf by running `freq.sh`,
+on the seed pileup data at /lustre/scratch/lczech/grenepipe-runs/ath-evo-seeds-pileups/
+The resultingfile is called frequency.csv, and was edited to contain a combined chrom_pos column
+(also done in the shell script).
 
-We first tried to extract freqs from the 1001g from scratch, but plink did not yield a file with positions
-in them, so instead we used an existing file from ~/safedata/1001g/plink.frq, in the hope that this is correct.
-See below for the plink commands we tried.
+The 1001g frequencies were created by Moi using plink, and are originally at ~/safedata/1001g/plink.frq
+We copied this file over to here as plink.frq
 
-1000g frews were extracted with plink, using
-plink --freq --vcf 1001genomes_snp-short-indel_only_ACGTN.vcf.gz
-in the ~/safedata/1001g/vcf data dir
-The resulting file is called plink.frq
+The 231g frequencies were also created by Moi using plink, and are originally at ~/safedata/ath_evo/grenephase1/data-big
+We copied them over here as 231g.frq
 
-We also tried using the plink option --biallelic-only, in order to filter out non biallelic snps.
-But that somehow does not work, and instead gives a table without position information,
-but still containing non-biallelic parts... No idea what this is about.
-
-231g copied from ~/safedata/ath_evo/grenephase1/data-big
+Then, using the plot.R the two scatter plots were created.
--- freq_seeds_vs_1001g/plot.R
+++ freq_seeds_vs_1001g/plot.R
@@ -3,44 +3,78 @@ library(ggplot2)
 library(cowplot)
 theme_set(theme_cowplot())
 
+print("reading tables")
+
 # Read the tables. Ours has proper tabs as delimiters,
 # the plink ones just uses some arbitrary spaces between columns,
 # so we need to set the sep accordingly.
 # Naming: seeds (obviously), 1001g (One Thousand One G) = otog,
 # and 231g (Two Three One G) = ttog
-seeds <- read.csv( "seed_freqs.csv", sep="\t" )
+seeds <- read.csv( "frequency.csv", sep="\t" )
 otog <- read.csv( "plink.frq", sep="" )
 ttog <- read.csv( "231g.frq", sep="" )
+
+# Test cases
+#seeds <- read.csv( "head_seeds.csv", sep="\t" )
+#otog <- read.csv( "head_plink.frq", sep="" )
+#ttog <- read.csv( "head_231g.frq", sep="" )
 
 # See if all is good
 #head(seeds)
 #head(otog)
 #head(ttog)
 
+print("computing frequencies and joined tabled")
+
+# The seeds freqs are unfolded, but the 1001g and 231g are not,
+# so we havel to fold the seeds as well.
+index <- seeds$TOTAL.FREQ > 0.5
+seeds$TOTAL.FREQ[index] <- 1.0 - seeds$TOTAL.FREQ[index]
+#seeds[seeds$TOTAL.FREQ > 0.5]$TOTAL.FREQ <- 1.0 - seeds[seeds$TOTAL.FREQ > 0.5]$TOTAL.FREQ
+
 # Join seeds with the two others, using the SNP column to only select
 # those rows that appear in both tables, omitting all other rows,
 # and filter out all NAN rows.
-joined_otog <- merge( seeds, otog, by.x="SNP", by.y="SNP", all=FALSE)
+joined_otog <- merge( seeds, otog, by.x="CHROM_POS", by.y="SNP", all=FALSE)
 joined_otog <- na.omit(joined_otog)
-joined_ttog <- merge( seeds, ttog, by.x="SNP", by.y="SNP", all=FALSE)
+joined_ttog <- merge( seeds, ttog, by.x="CHROM_POS", by.y="SNP", all=FALSE)
 joined_ttog <- na.omit(joined_ttog)
 
 # See if things are still good
-#head(joined_otog)
-#head(joined_ttog)
+#print(head(joined_otog))
+#print(head(joined_ttog))
+
+print("plotting")
 
 # Tests...
 #plot(joined$"MAF.x", joined$"MAF.y", main="Seed vs 1001g AFs",  xlab="Seed MAF", ylab="1001g MAF", pch=19)
 #ggplot(joined_otog, aes(x=MAF.x, y=MAF.y)) + geom_point(alpha = 1/100) #+ geom_smooth(method=lm)
 
 # Plot otog
-ggplot(joined_otog, aes(x=MAF.x, y=MAF.y)) + geom_point(alpha = 1/100) + geom_abline(slope = 1, colour="blue") + xlab("Seed MAF") + ylab("1001g MAF")
+ggplot(joined_otog, aes(x=TOTAL.FREQ, y=MAF)) + 
+    geom_hline( yintercept=0, color="gray") +
+    geom_vline( xintercept=0, color="gray") +
+    geom_abline(slope = 1, colour="blue") +
+    geom_point(alpha = 1/100, size=0.1) + 
+    xlab("Seed MAF") + 
+    ylab("1001g MAF") +
+    xlim(0, 0.5) +
+    ylim(0, 0.5)
+
 #show()
-# ggsave cannot do pixels. stupid. let's try to find a size that works.
-ggsave("seeds_vs_1001g_freqs.png")
-ggsave("seeds_vs_1001g_freqs_large.png", width=16, height=15)
+ggsave("seeds_vs_1001g_freqs.png", bg="white")
+#ggsave("seeds_vs_1001g_freqs_large.png", width=16, height=15)
 
 # Plot ttog
-ggplot(joined_ttog, aes(x=MAF.x, y=MAF.y)) + geom_point(alpha = 1/100) + geom_abline(slope = 1, colour="blue") + xlab("Seed MAF") + ylab("231g MAF")
-ggsave("seeds_vs_231g_freqs.png")
-ggsave("seeds_vs_231g_freqs_large.png", width=16, height=15)
+ggplot(joined_ttog, aes(x=TOTAL.FREQ, y=MAF)) + 
+    geom_hline( yintercept=0, color="gray") +
+    geom_vline( xintercept=0, color="gray") +
+    geom_point(alpha = 1/100, size=0.1) + 
+    geom_abline(slope = 1, colour="blue") +
+    xlab("Seed MAF") + 
+    ylab("231g MAF")  +
+    xlim(0, 0.5) +
+    ylim(0, 0.5)
+
+ggsave("seeds_vs_231g_freqs.png", bg="white")
+#ggsave("seeds_vs_231g_freqs_large.png", width=16, height=15)
